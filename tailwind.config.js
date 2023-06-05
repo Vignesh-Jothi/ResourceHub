a65b7d1e1c46cfd8249e8058f30571c635444c01
jvignesh <jvignesh609@gmail.com>
2023-06-05 13:52:40 +0530

    New feat

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   'hero-pattern': "url('public/Background.svg')",
+      // }
+    },
+  },
+  plugins: [],
+}
