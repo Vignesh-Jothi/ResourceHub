8dbfd81eef6ca3ed89de52f1ecbaaeadf92f4ae1
jvignesh <jvignesh609@gmail.com>
2023-06-20 21:37:30 +0530

    Update Changes

--- src/nav.js
+++ src/nav.js
@@ -1,8 +1,6 @@
 const button = document.querySelector('#menu-button'); // Hamburger Icon
 const menu = document.querySelector('#menu'); // Menu
-const icon = document.querySelector('#icon');
 
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
-  icon.classList.toggle('hidden');
 });
